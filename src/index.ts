8adfe161b2eea54f061de9cbd227b3929ba784e9
sakaandplasticbank <99636740+sakaandplasticbank@users.noreply.github.com>
2025-11-27 07:12:31 +0800

    tweak

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,8 @@ var index_default = {
     if (country != null && country in countryMap) {
       const url = countryMap[country];
 		const baseUrl = new URL(request.url);
-      console.log(request.url);
-		if (url.pathname.startsWith('/individuals')) {
+      console.log(baseUrl);
+		if (baseUrl.pathname.startsWith('/individuals')) {
       return new Response('Response for specific path');
     }
     return new Response('Hello World');
